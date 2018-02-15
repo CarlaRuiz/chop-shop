7cb77c65e0feb5c1f3a53fa5b59b2693349fa33e
Carla Ruiz - ED <carlaruiz-ed@carlas-macbook-air.local>
2018-02-15 11:16:54 -0800

    Initial Commit

---
+++ chopshop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  chopshop
+//
+//  Created by Carla Ruiz - ED  on 2/15/18.
+//  Copyright © 2018 Carla Ruiz - ED . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
